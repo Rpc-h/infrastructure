072cf84013d3b615106752fd0b4614db548ddc74
Martins Eglitis <martins.eglitis@woven-planet.global>
2023-02-01 11:03:39 +0200

    Create initial cluster on GCP

---
+++ day-0/apis.tf
@@ -0,0 +1,17 @@
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "dns" {
+  service = "dns.googleapis.com"
+
+  disable_dependent_services = true
+}
---
+++ day-1/variables.tf
@@ -0,0 +1,9 @@
+variable "google_project" {
+  type        = string
+  description = "The ID of the GCP project"
+}
+
+variable "google_region" {
+  type        = string
+  description = "The default GCP region for resource placement"
+}
