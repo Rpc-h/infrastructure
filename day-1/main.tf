f857eb51d40a2574ac0f937c8788746e39ef5a0d
Martins Eglitis <sitilge@gmail.com>
2023-02-15 11:38:40 +0200

    Add a comment regarding IAM binding

--- day-1/main.tf
+++ day-1/main.tf
@@ -1,5 +1,4 @@
-#TODO - https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam#google_project_iam_binding
-#Looks like GCP is yolo deleting or assigning service accounts to roles...
+#See the reason behind it here: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam#google_project_iam_binding
 resource "google_project_iam_binding" "main" {
   members = [
     "serviceAccount:${google_service_account.cert_manager.email}",
