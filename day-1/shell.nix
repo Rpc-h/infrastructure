f0b66f2add563d7398eb84adcc31ac810757744f
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-09-25 09:31:46 +0200

    initial flake setup

---
+++ day-1/shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  linuxPkgs = with pkgs; lib.optional stdenv.isLinux (
+    inotify-tools
+  );
+  macosPkgs = with pkgs; lib.optional stdenv.isDarwin (
+    with darwin.apple_sdk.frameworks; [
+      CoreFoundation
+      CoreServices
+    ]
+  );
+in
+with pkgs;
+mkShell {
+  nativeBuildInputs = [
+    ansible
+    # custom pkg groups
+    linuxPkgs
+    macosPkgs
+  ];
+}
